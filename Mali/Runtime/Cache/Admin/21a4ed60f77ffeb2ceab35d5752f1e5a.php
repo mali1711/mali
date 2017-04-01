9136109df3a98d22e4e40d9cbfde6773bf6ae3e1
mali <mali1711@163.com>
2017-04-01 16:23:37 +0800

    添加的新数据

--- Mali/Runtime/Cache/Admin/21a4ed60f77ffeb2ceab35d5752f1e5a.php
+++ Mali/Runtime/Cache/Admin/21a4ed60f77ffeb2ceab35d5752f1e5a.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>首页 - 素材火 Admin</title>
+    <title>树型图 - 素材火 Admin</title>
 
-    <meta name="description" content="overview &amp; stats" />
+    <meta name="description" content="with selectable items(single &amp; multiple) and custom icons" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!-- basic styles -->
@@ -216,7 +216,7 @@
                         <li>
                             <a href="#">
                                 <img src="/www/mali/Public/Style_Admin/assets/avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
-										<span class="msg-body">
+                                <span class="msg-body">
 											<span class="msg-title">
 												<span class="blue">Alex:</span>
 												Ciao sociis natoque penatibus et auctor ...
@@ -233,7 +233,7 @@
                         <li>
                             <a href="#">
                                 <img src="/www/mali/Public/Style_Admin/assets/avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
-										<span class="msg-body">
+                                <span class="msg-body">
 											<span class="msg-title">
 												<span class="blue">Susan:</span>
 												Vestibulum id ligula porta felis euismod ...
@@ -250,7 +250,7 @@
                         <li>
                             <a href="#">
                                 <img src="/www/mali/Public/Style_Admin/assets/avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
-										<span class="msg-body">
+                                <span class="msg-body">
 											<span class="msg-title">
 												<span class="blue">Bob:</span>
 												Nullam quis risus eget urna mollis ornare ...
@@ -276,7 +276,7 @@
                 <li class="light-blue">
                     <a data-toggle="dropdown" href="#" class="dropdown-toggle">
                         <img class="nav-user-photo" src="/www/mali/Public/Style_Admin/assets/avatars/user.jpg" alt="Jason's Photo" />
-								<span class="user-info">
+                        <span class="user-info">
 									<small>Welcome,</small>
 									Jason
 								</span>
@@ -360,7 +360,7 @@
             </div><!-- #sidebar-shortcuts -->
 
             <ul class="nav nav-list">
-                <li class="active">
+                <li>
                     <a href="index.html">
                         <i class="icon-dashboard"></i>
                         <span class="menu-text"> Dashboard </span>
@@ -374,7 +374,7 @@
                     </a>
                 </li>
 
-                <li>
+                <li class="active open">
                     <a href="#" class="dropdown-toggle">
                         <i class="icon-desktop"></i>
                         <span class="menu-text"> UI Elements </span>
@@ -397,7 +397,7 @@
                             </a>
                         </li>
 
-                        <li>
+                        <li class="active">
                             <a href="treeview.html">
                                 <i class="icon-double-angle-right"></i>
                                 Treeview
@@ -538,7 +538,7 @@
                     <a href="calendar.html">
                         <i class="icon-calendar"></i>
 
-								<span class="menu-text">
+                        <span class="menu-text">
 									Calendar
 									<span class="badge badge-transparent tooltip-error" title="2&nbsp;Important&nbsp;Events">
 										<i class="icon-warning-sign red bigger-130"></i>
@@ -611,7 +611,7 @@
                     <a href="#" class="dropdown-toggle">
                         <i class="icon-file-alt"></i>
 
-								<span class="menu-text">
+                        <span class="menu-text">
 									Other Pages
 									<span class="badge badge-primary ">5</span>
 								</span>
@@ -666,6 +666,8 @@
                 try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
             </script>
         </div>
+
+        <!--内容显示-->
         
 	<div class="main-content">
 	<div class="breadcrumbs" id="breadcrumbs">
@@ -1836,6 +1838,7 @@
 	</div><!-- /.page-content -->
 </div><!-- /.main-content -->
 
+        <!--内容显示end-->
         <div class="ace-settings-container" id="ace-settings-container">
             <div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
                 <i class="icon-cog bigger-150"></i>
@@ -1895,37 +1898,27 @@
 <!--[if !IE]> -->
 
 <script type="text/javascript">
-    window.jQuery || document.write("<script src='assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+    window.jQuery || document.write("<script src='/www/mali/Public/Style_Admin/assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
 </script>
 
 <!-- <![endif]-->
 
 <!--[if IE]>
 <script type="text/javascript">
-    window.jQuery || document.write("<script src='assets/js/jquery-1.10.2.min.js'>"+"<"+"/script>");
+    window.jQuery || document.write("<script src='/www/mali/Public/Style_Admin/assets/js/jquery-1.10.2.min.js'>"+"<"+"/script>");
 </script>
 <![endif]-->
 
 <script type="text/javascript">
-    if("ontouchend" in document) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+    if("ontouchend" in document) document.write("<script src='/www/mali/Public/Style_Admin/assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
 </script>
 <script src="/www/mali/Public/Style_Admin/assets/js/bootstrap.min.js"></script>
 <script src="/www/mali/Public/Style_Admin/assets/js/typeahead-bs2.min.js"></script>
 
 <!-- page specific plugin scripts -->
 
-<!--[if lte IE 8]>
-<script src="/www/mali/Public/Style_Admin/assets/js/excanvas.min.js"></script>
-<![endif]-->
-
-<script src="/www/mali/Public/Style_Admin/assets/js/jquery-ui-1.10.3.custom.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/jquery.ui.touch-punch.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/jquery.slimscroll.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/jquery.easy-pie-chart.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/jquery.sparkline.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/flot/jquery.flot.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/flot/jquery.flot.pie.min.js"></script>
-<script src="/www/mali/Public/Style_Admin/assets/js/flot/jquery.flot.resize.min.js"></script>
+<script src="/www/mali/Public/Style_Admin/assets/js/fuelux/data/fuelux.tree-sampledata.js"></script>
+<script src="/www/mali/Public/Style_Admin/assets/js/fuelux/fuelux.tree.min.js"></script>
 
 <!-- ace scripts -->
 
@@ -1935,197 +1928,45 @@
 <!-- inline scripts related to this page -->
 
 <script type="text/javascript">
-    jQuery(function($) {
-        $('.easy-pie-chart.percentage').each(function(){
-            var $box = $(this).closest('.infobox');
-            var barColor = $(this).data('color') || (!$box.hasClass('infobox-dark') ? $box.css('color') : 'rgba(255,255,255,0.95)');
-            var trackColor = barColor == 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)' : '#E2E2E2';
-            var size = parseInt($(this).data('size')) || 50;
-            $(this).easyPieChart({
-                barColor: barColor,
-                trackColor: trackColor,
-                scaleColor: false,
-                lineCap: 'butt',
-                lineWidth: parseInt(size/10),
-                animate: /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase()) ? false : 1000,
-                size: size
-            });
-        })
+    jQuery(function($){
 
-        $('.sparkline').each(function(){
-            var $box = $(this).closest('.infobox');
-            var barColor = !$box.hasClass('infobox-dark') ? $box.css('color') : '#FFF';
-            $(this).sparkline('html', {tagValuesAttribute:'data-values', type: 'bar', barColor: barColor , chartRangeMin:$(this).data('min') || 0} );
+        $('#tree1').ace_tree({
+            dataSource: treeDataSource ,
+            multiSelect:true,
+            loadingHTML:'<div class="tree-loading"><i class="icon-refresh icon-spin blue"></i></div>',
+            'open-icon' : 'icon-minus',
+            'close-icon' : 'icon-plus',
+            'selectable' : true,
+            'selected-icon' : 'icon-ok',
+            'unselected-icon' : 'icon-remove'
+        });
+
+        $('#tree2').ace_tree({
+            dataSource: treeDataSource2 ,
+            loadingHTML:'<div class="tree-loading"><i class="icon-refresh icon-spin blue"></i></div>',
+            'open-icon' : 'icon-folder-open',
+            'close-icon' : 'icon-folder-close',
+            'selectable' : false,
+            'selected-icon' : null,
+            'unselected-icon' : null
         });
 
 
-
-
-        var placeholder = $('#piechart-placeholder').css({'width':'90%' , 'min-height':'150px'});
-        var data = [
-            { label: "social networks",  data: 38.7, color: "#68BC31"},
-            { label: "search engines",  data: 24.5, color: "#2091CF"},
-            { label: "ad campaigns",  data: 8.2, color: "#AF4E96"},
-            { label: "direct traffic",  data: 18.6, color: "#DA5430"},
-            { label: "other",  data: 10, color: "#FEE074"}
-        ]
-        function drawPieChart(placeholder, data, position) {
-            $.plot(placeholder, data, {
-                series: {
-                    pie: {
-                        show: true,
-                        tilt:0.8,
-                        highlight: {
-                            opacity: 0.25
-                        },
-                        stroke: {
-                            color: '#fff',
-                            width: 2
-                        },
-                        startAngle: 2
-                    }
-                },
-                legend: {
-                    show: true,
-                    position: position || "ne",
-                    labelBoxBorderColor: null,
-                    margin:[-30,15]
-                }
-                ,
-                grid: {
-                    hoverable: true,
-                    clickable: true
-                }
-            })
-        }
-        drawPieChart(placeholder, data);
 
         /**
-         we saved the drawing function and the data to redraw with different position later when switching to RTL mode dynamically
-         so that's not needed actually.
+         $('#tree1').on('loaded', function (evt, data) {
+		});
+
+         $('#tree1').on('opened', function (evt, data) {
+		});
+
+         $('#tree1').on('closed', function (evt, data) {
+		});
+
+         $('#tree1').on('selected', function (evt, data) {
+		});
          */
-        placeholder.data('chart', data);
-        placeholder.data('draw', drawPieChart);
-
-
-
-        var $tooltip = $("<div class='tooltip top in'><div class='tooltip-inner'></div></div>").hide().appendTo('body');
-        var previousPoint = null;
-
-        placeholder.on('plothover', function (event, pos, item) {
-            if(item) {
-                if (previousPoint != item.seriesIndex) {
-                    previousPoint = item.seriesIndex;
-                    var tip = item.series['label'] + " : " + item.series['percent']+'%';
-                    $tooltip.show().children(0).text(tip);
-                }
-                $tooltip.css({top:pos.pageY + 10, left:pos.pageX + 10});
-            } else {
-                $tooltip.hide();
-                previousPoint = null;
-            }
-
-        });
-
-
-
-
-
-
-        var d1 = [];
-        for (var i = 0; i < Math.PI * 2; i += 0.5) {
-            d1.push([i, Math.sin(i)]);
-        }
-
-        var d2 = [];
-        for (var i = 0; i < Math.PI * 2; i += 0.5) {
-            d2.push([i, Math.cos(i)]);
-        }
-
-        var d3 = [];
-        for (var i = 0; i < Math.PI * 2; i += 0.2) {
-            d3.push([i, Math.tan(i)]);
-        }
-
-
-        var sales_charts = $('#sales-charts').css({'width':'100%' , 'height':'220px'});
-        $.plot("#sales-charts", [
-            { label: "Domains", data: d1 },
-            { label: "Hosting", data: d2 },
-            { label: "Services", data: d3 }
-        ], {
-            hoverable: true,
-            shadowSize: 0,
-            series: {
-                lines: { show: true },
-                points: { show: true }
-            },
-            xaxis: {
-                tickLength: 0
-            },
-            yaxis: {
-                ticks: 10,
-                min: -2,
-                max: 2,
-                tickDecimals: 3
-            },
-            grid: {
-                backgroundColor: { colors: [ "#fff", "#fff" ] },
-                borderWidth: 1,
-                borderColor:'#555'
-            }
-        });
-
-
-        $('#recent-box [data-rel="tooltip"]').tooltip({placement: tooltip_placement});
-        function tooltip_placement(context, source) {
-            var $source = $(source);
-            var $parent = $source.closest('.tab-content')
-            var off1 = $parent.offset();
-            var w1 = $parent.width();
-
-            var off2 = $source.offset();
-            var w2 = $source.width();
-
-            if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
-            return 'left';
-        }
-
-
-        $('.dialogs,.comments').slimScroll({
-            height: '300px'
-        });
-
-
-        //Android's default browser somehow is confused when tapping on label which will lead to dragging the task
-        //so disable dragging when clicking on label
-        var agent = navigator.userAgent.toLowerCase();
-        if("ontouchstart" in document && /applewebkit/.test(agent) && /android/.test(agent))
-            $('#tasks').on('touchstart', function(e){
-                var li = $(e.target).closest('#tasks li');
-                if(li.length == 0)return;
-                var label = li.find('label.inline').get(0);
-                if(label == e.target || $.contains(label, e.target)) e.stopImmediatePropagation() ;
-            });
-
-        $('#tasks').sortable({
-                    opacity:0.8,
-                    revert:true,
-                    forceHelperSize:true,
-                    placeholder: 'draggable-placeholder',
-                    forcePlaceholderSize:true,
-                    tolerance:'pointer',
-                    stop: function( event, ui ) 
-                }
-        );
-        $('#tasks').disableSelection();
-        $('#tasks input:checkbox').removeAttr('checked').on('click', function(){
-            if(this.checked) $(this).closest('li').addClass('selected');
-            else $(this).closest('li').removeClass('selected');
-        });
-
-
-    })
+    });
 </script>
 </body>
 </html>
