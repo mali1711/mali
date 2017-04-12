21c69eb738e999ccff302236e030797703be3f6a
mali <mali1711@163.com>
2017-04-12 16:44:01 +0800

    begin MyFootsteps

---
+++ Mali/Admin/Controller/MyFootstepsController.class.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * 文章信息控制器
+ * */
+namespace Admin\Controller;
+use Think\Controller;
+class MyFootstepsController extends Controller {
+
+    /*
+     * jump  to MyFootsteps list page
+     * */
+    public function MyFootstepsList()
+    {
+        
+    }
+
+    /*
+     * 
+     * */
+    public function addMyFootsteps()
+    {
+        
+    }
+
+    /*
+     * post add MyFootsteps
+     * */
+    public function postMyFootsteps()
+    {
+        
+    }
+    
+    public function delMyFootsteps()
+    {
+
+    }
+}
